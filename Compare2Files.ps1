7471dd8b5f7e2cc4dc5f7f4a41a9d6cca776e79a
William B Hamilton <williamhamilton2013@gmail.com>
2021-03-16 09:10:40 -0400

    Get Software and Compare 2 Files

---
+++ Compare2Files.ps1
@@ -0,0 +1,4 @@
+﻿$theway = Read-Host -Prompt "Where are these files located? They must be in the same Folder, and this file will output to that folder."
+$PC1 = Read-Host -Prompt "What is the name of file 1? Default name will be the Hostname of the PC"
+$PC2 = Read-Host -Prompt "What is the name of file 2? Default name will be the Hostname of the PC"
+Compare-Object -ReferenceObject (Get-Content $theway\$PC1.txt) -DifferenceObject (Get-Content $theway\$PC2.txt) |out-file $theway\$PC1"___"$PC2" Difference Report".txt
---
+++ Get-Installed-Apps.ps1
@@ -0,0 +1,3 @@
+﻿$theway = Read-Host -Prompt "Where would you like this saved?"
+get-package | Select-Object Name,version | Format-Table –AutoSize | out-file $theway\$env:COMPUTERNAME.txt
+$ErrorActionPreference = "Stop"
